1fd08c3a972cd3dfe7b070d9a605c2062dbdcdcb
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2020-04-01 18:25:22 +0200

    remove external deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { History } from 'history';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { History } from 'history';
 
 type FrontEnd = {
   register: (container: string, history: History) => void;
